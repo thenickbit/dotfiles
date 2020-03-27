4074a2f856e836541a9f4883ae7e84c557b7a70f
Nicolas Chartiot <nicolaschartiot@gmail.com>
2020-03-27 16:57:24 -0300

    New backup 2020-03-27 16:57:24

---
+++ backupDotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# check to see is git command line installed in this machine
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+# copy other dot files 
+cp  $HOME/{.zshrc,.profile} .
+
+# Check git status
+gs="$(git status | grep -i "modified")"
+# echo "${gs}"
+
+# If there is a new change
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+# push to Github
+git add -u;
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin master
